6ad15266bb1d7843fb90f9c940b57fa86db0c5f8
mugagga-kasajja <mugaggakasajja@gmail.com>
2020-09-24 11:48:16 +0300

    Added cards to wards screen

--- src/navigation/myScreens/WardsScreen.js
+++ src/navigation/myScreens/WardsScreen.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import * as React from 'react';
+import {View} from 'react-native';
+import {Avatar, Button, Card, Title, Paragraph} from 'react-native-paper';
 
-const WardsScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Wards screen</Text>
+const LeftContent = (props) => <Avatar.Icon {...props} icon="heart" />;
+const RightContent = (props) => <Avatar.Icon {...props} icon="home" />;
+
+const WardsScreen = () => (
+  <View>
+    <View>
+      <Card>
+        <Card.Title
+          title="Marternity Ward"
+          subtitle="Pregnancy"
+          left={RightContent}
+        />
+        <Card.Content>
+          <Title>8 beds</Title>
+          <Paragraph>2 patients</Paragraph>
+        </Card.Content>
+      </Card>
     </View>
-  );
-};
+    <View>
+      <Card>
+        <Card.Title
+          title="Cardiology Ward"
+          subtitle="Heart"
+          left={LeftContent}
+        />
+        <Card.Content>
+          <Title>8 beds</Title>
+          <Paragraph>2 patients</Paragraph>
+        </Card.Content>
+      </Card>
+    </View>
+    <View>
+      <Card>
+        <Card.Title
+          title="Intensive Care Unit"
+          subtitle="Critical Care"
+          left={LeftContent}
+        />
+        <Card.Content>
+          <Title>8 beds</Title>
+          <Paragraph>2 patients</Paragraph>
+        </Card.Content>
+      </Card>
+    </View>
+  </View>
+);
 
 export default WardsScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
