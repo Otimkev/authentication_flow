7591d588af24e45605939cb128e7e8ce8d55063e
mugagga-kasajja <mugaggakasajja@gmail.com>
2020-09-26 09:20:05 +0300

    Working on chat screen

--- src/navigation/myScreens/ChatScreen.js
+++ src/navigation/myScreens/ChatScreen.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {FloatingAction} from 'react-native-floating-action';
+import Icon from 'react-native-vector-icons/Fontisto';
 
-const ChatScreen = () => {
+const actions = [
+  {
+    text: 'Broadcast',
+    // icon: <Icon name="home" color="#007360" />,
+    name: 'bt_accessibility',
+    position: 2,
+  },
+  {
+    text: 'New Group',
+    icon: require('../../assets/img/Crit.png'),
+    name: 'bt_language',
+    position: 1,
+  },
+  {
+    text: 'Specialists',
+    // icon: require('./images/ic_room_white.png'),
+    name: 'bt_room',
+    position: 3,
+  },
+  {
+    text: 'New Chat',
+    name: 'bt_videocam',
+    position: 4,
+  },
+];
+
+const ChatScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Chat screen</Text>
+      <FloatingAction
+        actions={actions}
+        onPressItem={() => {
+          navigation.navigate('New Chat');
+        }}
+      />
     </View>
   );
 };
