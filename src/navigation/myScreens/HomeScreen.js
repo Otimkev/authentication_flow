0990f3afddcff27b3c1d28c4425e65ec53bb86ae
mugagga-kasajja <mugaggakasajja@gmail.com>
2020-09-30 10:37:33 +0300

    Redefining home screen

--- src/navigation/myScreens/HomeScreen.js
+++ src/navigation/myScreens/HomeScreen.js
@@ -1,61 +1,22 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import Dashboard from 'react-native-dashboard';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Attention from '../../components/AttentionComponent';
+import {globalStyles} from '../../styles/Global';
 
-const items = [
-  {
-    name: 'To do',
-    background: '#007360',
-    icon: 'calendar-check-o',
-  },
-  {
-    name: 'On Duty',
-    background: '#007360',
-    icon: 'gratipay',
-  },
-  {
-    name: 'In Patients',
-    background: '#007360',
-    icon: 'heart',
-  },
-  {
-    name: 'Medical Officers',
-    background: '#007360',
-    icon: 'users',
-  },
-  {
-    name: 'Inventory',
-    background: '#007360',
-    icon: 'group',
-  },
-  {
-    name: 'Calendar',
-    background: '#007360',
-    icon: 'calendar',
-  },
-];
+const HomeScreen = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Attention />
+    </View>
+  );
+};
 
-export default class HomeScreen extends Component {
-  _card = (el) => {
-    console.log('Card: ' + el.name);
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Dashboard
-          items={items}
-          background={true}
-          card={this._card}
-          column={2}
-        />
-      </View>
-    );
-  }
-}
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
-  },
+  // container: {
+  //   flex: 1,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
