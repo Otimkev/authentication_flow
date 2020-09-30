272dd892e7920663b52b19b952bd116cfdc0647f
mugagga-kasajja <mugaggakasajja@gmail.com>
2020-09-30 09:39:38 +0300

    Rededining the Wards Screen

--- src/navigation/myScreens/HomeScreen.js
+++ src/navigation/myScreens/HomeScreen.js
@@ -1,20 +1,61 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Dashboard from 'react-native-dashboard';
 
-const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Home screen</Text>
-    </View>
-  );
-};
+const items = [
+  {
+    name: 'To do',
+    background: '#007360',
+    icon: 'calendar-check-o',
+  },
+  {
+    name: 'On Duty',
+    background: '#007360',
+    icon: 'gratipay',
+  },
+  {
+    name: 'In Patients',
+    background: '#007360',
+    icon: 'heart',
+  },
+  {
+    name: 'Medical Officers',
+    background: '#007360',
+    icon: 'users',
+  },
+  {
+    name: 'Inventory',
+    background: '#007360',
+    icon: 'group',
+  },
+  {
+    name: 'Calendar',
+    background: '#007360',
+    icon: 'calendar',
+  },
+];
 
-export default HomeScreen;
+export default class HomeScreen extends Component {
+  _card = (el) => {
+    console.log('Card: ' + el.name);
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Dashboard
+          items={items}
+          background={true}
+          card={this._card}
+          column={2}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
 });
