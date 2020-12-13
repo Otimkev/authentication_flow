b99e8a32e7ec9220cf33237bbb70d91d0d34cd0f
Otimkev <kevinotim4@gmail.com>
2020-12-13 10:58:30 +0300

    edite the test list file

--- src/utils/config/Urls.js
+++ src/utils/config/Urls.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://192.168.43.252:3001/api/v1/';
-export const BASE_URL = 'http://192.168.43.252:3001/';
+export const API_URL = 'http://192.168.43.253:3001/api/v1/';
+export const BASE_URL = 'http://192.168.43.253:3001/';
