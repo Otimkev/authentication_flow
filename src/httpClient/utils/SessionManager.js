d37f3a02ec855fe9530d310533320cae3e199e49
Otimkev <kevinotim4@gmail.com>
2020-12-14 23:36:40 +0300

    removed the httpclient folder

--- src/httpClient/utils/SessionManager.js
+++
@@ -1,7 +0,0 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import React, {Component} from 'react';
-
-class SessionManager extends Component {
-}
-
-export default new SessionManager();
